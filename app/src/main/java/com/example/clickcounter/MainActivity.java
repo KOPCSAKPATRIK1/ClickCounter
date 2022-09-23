214f045be4cae9c4f7c94d783cc975340e6e3a17
Kopcsak Patrik Bence <kopcsakpatrik1@gmail.com>
2022-09-23 09:15:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button pluszGomb;
+    Button minuszGomb;
+    TextView output;
+    RelativeLayout layout;
+    int szamlalo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+        pluszGomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo++;
+                output.setText(szamlalo);
+            }
+        });
+
+        minuszGomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo--;
+                output.setText(szamlalo);
+            }
+        });
+
+        if (szamlalo == 0)
+        {
+            output.setTextColor(Color.rgb(0, 0, 255));
+        }
+        else if (szamlalo > 0)
+        {
+            output.setTextColor(Color.rgb(255, 0, 0));
+        }
+        else
+        {
+            output.setTextColor(Color.rgb(0, 255, 0));
+        }
+
+    }
+
+    private void init()
+    {
+        szamlalo = 0;
+        layout = findViewById(R.id.layout);
+        pluszGomb = findViewById(R.id.pluszGomb);
+        minuszGomb = findViewById(R.id.minuszGomb);
+        output = findViewById(R.id.szamolo);
+    }
+
+}
+
